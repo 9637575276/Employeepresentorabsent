2014693d056118463ebd8ed6dd4a5d93a3a5b0b9
Shubham Mule <shubhambmule@gmail.com>
2021-09-03 08:08:02 +0530

    added new

---
+++ AttendanceEmp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Employeewage
+    {
+        class Program
+        {
+            static void Main(string[] args)
+
+            {
+                int IS_PRESENT = 1;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == IS_PRESENT)
+
+                    Console.WriteLine("Employee is present");
+
+                else
+
+                    Console.WriteLine("Employee is absent");
+
+            }
+        }
+    }
+
